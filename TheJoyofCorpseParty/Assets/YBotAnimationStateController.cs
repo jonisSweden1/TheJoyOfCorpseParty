f667125a69213e6b6c560900bab1184827565d5a
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2025-06-22 15:20:26 +0200

    Added blend tree

--- TheJoyofCorpseParty/Assets/YBotAnimationStateController.cs
+++ TheJoyofCorpseParty/Assets/YBotAnimationStateController.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class YBotAnimationStateController : MonoBehaviour
 {
+    private Animator _animator;
+    int isWalkingHash;
+
+    [SerializeField]
+    private InputActionReference actionReference;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+
+        actionReference.action.performed += Action_performed;
+        actionReference.action.canceled += Action_canceled;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Action_canceled(InputAction.CallbackContext obj)
     {
-        
+        SetWalking(false);
+    }
+
+    private void SetWalking(bool isWalking)
+    {
+        _animator.SetBool(isWalkingHash, isWalking);
+    }
+
+    private void Action_performed(InputAction.CallbackContext obj)
+    {
+        SetWalking(true);
     }
 }
