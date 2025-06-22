f3eccc850fe125b2453e6d10bc3b141b54ea5db6
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2025-06-22 15:20:54 +0200

    Little mistake...

--- TheJoyofCorpseParty/Assets/YBotAnimationStateController.cs
+++ TheJoyofCorpseParty/Assets/YBotAnimationStateController.cs
@@ -14,23 +14,5 @@ public class YBotAnimationStateController : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
-
-        actionReference.action.performed += Action_performed;
-        actionReference.action.canceled += Action_canceled;
-    }
-
-    private void Action_canceled(InputAction.CallbackContext obj)
-    {
-        SetWalking(false);
-    }
-
-    private void SetWalking(bool isWalking)
-    {
-        _animator.SetBool(isWalkingHash, isWalking);
-    }
-
-    private void Action_performed(InputAction.CallbackContext obj)
-    {
-        SetWalking(true);
     }
 }
