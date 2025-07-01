e25dc5c5431c8583a0a5007bf6b13e2c17f8ccf4
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2025-07-01 12:59:40 +0200

    Removing States and using states in player movement instead. I'll refactor later

--- TheJoyofCorpseParty/Assets/Scripts/PlayerMovement.cs
+++ TheJoyofCorpseParty/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,6 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Input")]
-    [SerializeField]
-    private InputActionReference m_MoveInputReference;
-
     [Header("Movement")]
     float m_MoveSpeed;
 
@@ -16,6 +12,25 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField]
     private float m_GroundDrag;
+
+    [Header("Crounching")]
+
+    [SerializeField]
+    private float m_CrouchSpeed;
+
+    [SerializeField]
+    private float crouchYScale;
+    float startYScale;
+    
+    [Header("Keybinds")]
+    [SerializeField]
+    private InputActionReference m_MoveInputReference;
+
+    [SerializeField]
+    private InputActionReference m_SprintKey;
+
+    [SerializeField]
+    private InputActionReference m_CrouchKey;
 
     [SerializeField]
     private Transform m_Orientation;
@@ -28,18 +43,28 @@ public class PlayerMovement : MonoBehaviour
 
     PlayerGroundCheck groundCheck;
 
+    private MovementState state;
+
+    public enum MovementState
+    {
+        walking, sprinting, crouching
+    }
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
 
         groundCheck = GetComponent<PlayerGroundCheck>();
+
+        startYScale = transform.localScale.y;
     }
 
     private void Update()
     {
         GetInput();
         SpeedControl();
+        StateHandler();
 
         // Handle drag
         if (groundCheck.Grounded)
@@ -58,6 +83,40 @@ public class PlayerMovement : MonoBehaviour
         Vector2 input = m_MoveInputReference.action.ReadValue<Vector2>();
         horizontalInput = input.x;
         verticalInput = input.y;
+
+        if(m_CrouchKey.action.IsPressed())
+        {
+            transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
+            rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+        }
+        else
+        {
+            transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
+        }
+    }
+
+    private void StateHandler()
+    {
+        // Mode - Crouching
+        if(m_CrouchKey.action.IsPressed())
+        {
+            state = MovementState.crouching;
+            m_MoveSpeed = m_CrouchSpeed;
+        }
+
+        // Mode - Sprinting
+        if(groundCheck.Grounded && m_SprintKey.action.IsPressed())
+        {
+            state = MovementState.sprinting;
+            m_MoveSpeed = sprintSpeed;
+        }
+
+        // Mode - Walking
+        else if (groundCheck.Grounded)
+        {
+            state = MovementState.walking;
+            m_MoveSpeed = walkSpeed;
+        }
     }
 
     private void MovePlayer()
