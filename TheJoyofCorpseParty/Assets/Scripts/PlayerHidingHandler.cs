96e02ac5a8ce04394d1d1eabbe115321d74aac65
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2025-08-25 15:50:46 +0200

    Continued developing hiding system

--- TheJoyofCorpseParty/Assets/Scripts/PlayerHidingHandler.cs
+++ TheJoyofCorpseParty/Assets/Scripts/PlayerHidingHandler.cs
@@ -1,16 +1,53 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerHidingHandler : MonoBehaviour
 {
+    [SerializeField]
+    LayerMask hidingMask;
+
+    [SerializeField]
+    private float distance;
+
+    public static event Action<Transform> m_OnUse;
+
+    [Header("Keybinds")]
+    [SerializeField] private InputActionReference m_UseKey;
+
+    private bool isHidingSpotFound;
+    private Transform cameraPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        m_UseKey.action.performed += UseAction_performed;
+    }
+
+    private void UseAction_performed(InputAction.CallbackContext obj)
+    {
+        if(isHidingSpotFound)
+        {
+            m_OnUse.Invoke(cameraPos);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, hidingMask))
+        {
+            if (hit.collider.tag == "HideSpot")
+            {
+                cameraPos = hit.collider.transform.GetChild(0);
+                isHidingSpotFound = true;
+            }
+        }
+        else
+        {
+            isHidingSpotFound = false;
+        }
     }
 }
