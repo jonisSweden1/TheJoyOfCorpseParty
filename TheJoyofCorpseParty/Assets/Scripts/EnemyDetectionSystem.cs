0673735f0cb1e97d33b1d8c8536fa98ab275196b
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2025-10-06 20:22:59 +0200

    Added party tables

--- TheJoyofCorpseParty/Assets/Scripts/EnemyDetectionSystem.cs
+++ TheJoyofCorpseParty/Assets/Scripts/EnemyDetectionSystem.cs
@@ -42,14 +42,26 @@ public class EnemyDetectionSystem : MonoBehaviour
         if (rangeChecks.Length != 0)
         {
             Transform target = rangeChecks[0].transform;
+
             Vector3 directionToTarget = (target.position - transform.position).normalized;
 
             if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
             {
+                Debug.Log("Sees player");
+
                 float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                RaycastHit hit;
+
+                if (Physics.Raycast(transform.position, directionToTarget, out hit, distanceToTarget, obstacleMask))
+                {
+                    Debug.Log(hit.collider);
+                }
 
                 if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
                 {
+                    Debug.Log(hit.collider);
+
                     if(CanSeePlayer == false)
                     {
                         m_OnDetected.Invoke();
