165d7632b3eb411d61cae39df97cec780b5b95a8
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2025-11-14 15:42:34 +0100

    Implemented Player Security Room Camera Controller

---
+++ TheJoyofCorpseParty/Assets/PlayerSecurityRoomController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSecurityRoomController : MonoBehaviour
+{
+    [SerializeField]
+    private InputActionReference navigationController;
+
+    [SerializeField]
+    private float speedInNavigation = 0.0f;
+
+    private float rotationY = 0.0f;
+
+    private PlayerControllerState _currentState;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _currentState = PlayerControllerState.Forward;
+    }
+
+    private void OnEnable()
+    {
+        navigationController.action.performed += ActionLookBehindPerformed;
+    }
+
+    private void OnDisable()
+    {
+        navigationController.action.performed -= ActionLookBehindPerformed;
+    }
+
+    private void ActionLookBehindPerformed(InputAction.CallbackContext obj)
+    {
+        if(_currentState == PlayerControllerState.Forward)
+        {
+            _currentState = PlayerControllerState.Backward;
+        }
+        else
+        {
+            _currentState = PlayerControllerState.Forward;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_currentState == PlayerControllerState.Backward)
+        {
+            if(rotationY < 180)
+            {
+                rotationY += speedInNavigation * Time.deltaTime;
+
+                Quaternion rotation = Quaternion.Euler(0, rotationY, 0);
+
+                transform.localRotation = rotation;
+            }
+        }
+        else
+        {
+            if (rotationY > 0)
+            {
+                rotationY -= speedInNavigation * Time.deltaTime;
+
+                Quaternion rotation = Quaternion.Euler(0, rotationY, 0);
+
+                transform.localRotation = rotation;
+            }
+        }
+    }
+}
+
+public enum PlayerControllerState
+{
+    Forward, Backward
+}
