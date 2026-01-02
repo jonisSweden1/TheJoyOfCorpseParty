05d635eb6665d86880fce9deb86c1f7e84b420ba
jonisSweden1 <118064643+jonisSweden1@users.noreply.github.com>
2026-01-02 13:58:37 +0100

    Tested jumpscare screen

--- TheJoyofCorpseParty/Assets/Scripts/DeathPlayerInputDataManager.cs
+++ TheJoyofCorpseParty/Assets/Scripts/DeathPlayerInputDataManager.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class DeathPlayerInputDataManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private NightToShowData nightOneData;
+
+    [SerializeField]
+    private NightToShowData nightTwoData;
+
+    public void GoToPlayAgain()
     {
-        
+        LoadSceneByNightId();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadSceneByNightId()
     {
-        
+        string rawData = DataTransferToScene.message;
+        Debug.Log(rawData);
+        NightDataEnum dataEnum = NightDataEnum.None;
+        if (!string.IsNullOrEmpty(rawData))
+        {
+            object objectEnum;
+            if (System.Enum.TryParse(typeof(NightDataEnum), rawData, true, out objectEnum))
+            {
+                dataEnum = (NightDataEnum)objectEnum;
+            }
+        }
+        switch (dataEnum)
+        {
+            case NightDataEnum.Night1:
+                LevelManager.Instance.LoadScene(nightOneData.sceneBuildId);
+                break;
+            case NightDataEnum.Night2:
+                LevelManager.Instance.LoadScene(nightTwoData.sceneBuildId);
+                break;
+            default:
+                LevelManager.Instance.LoadScene(1);
+                break;
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        LevelManager.Instance.LoadScene(0);
     }
 }
